92e8fbb71ade657316d3f0f7057661e7eb7d7497
Fyrgeit <mans@peronilla.se>
2023-11-30 16:34:49 +0100

    created day 1 and edited day 0

--- 2023/00/index.js
+++ 2023/00/index.js
@@ -1,16 +1,12 @@
 console.log("Day 00 (test) of Advent of Code 2023!");
 console.log("(actually day 01 2022)");
 
-Bun
-    .file("input.txt")
-    .text()
-    .then(text => console.log(
-        text
-            .split("\n\n")
-            .map(monke => monke
-                .split("\n")
-                .reduce((acc, val) => Number(acc) + Number(val), 0))
-            .sort((a, b) => b - a)
-            .slice(0, 3)
-            .reduce((acc, val) => acc + val, 0)
-    ));
+console.log((await Bun.file("input.txt").text())
+    .split("\n\n")
+    .map(monke => monke
+        .split("\n")
+        .reduce((acc, val) => Number(acc) + Number(val), 0))
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((acc, val) => acc + val, 0)
+);
---
+++ 2023/01/index.js
@@ -0,0 +1,6 @@
+Bun
+    .file("input.txt")
+    .text()
+    .then(text => console.log(
+        text
+    ));

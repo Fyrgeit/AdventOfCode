b5f1c77c90c98a2fd256f89d64385ef805ac37d1
Fyrgeit <mans@peronilla.se>
2023-11-30 22:03:21 +0100

    lite fast, collapse funkar inte

--- 2022/15/index.js
+++ 2022/15/index.js
@@ -1,4 +1,4 @@
-const formated = (await Bun.file("input.txt").text())
+const formated = (await Bun.file("test.txt").text())
     .split("\n")
     .map(line => line
         .split("Sensor at ")[1]
@@ -23,9 +23,9 @@ const formated = (await Bun.file("input.txt").text())
         }
     });
 
-const widths = formated
+let widths = formated
     .map(el => {
-        let w = el.br - Math.abs(el.cy - 2000000);
+        let w = el.br - Math.abs(el.cy - 10);
         return {
             start: el.cx - w,
             end: el.cx + w
@@ -34,6 +34,60 @@ const widths = formated
     .filter(el => el.start <= el.end);
 
 console.dir(widths);
+
+for (let i = 0; i < 100; i++) {
+    //console.log(i + "===================");
+    let arrcpy = JSON.parse(JSON.stringify(widths));
+    widths = [];
+    
+    arrcpy.forEach(pair => {
+        collapse(widths, pair);
+        //console.log(widths.length);
+    });
+}
+
+console.log(widths);
+console.log(widths
+    .map(w => {return w.end - w.start + 1})
+    .reduce((sum, val) => sum + val, 0)
+);
+
+function collapse(pairSum, newPair) {
+    let added = false;
+    
+    pairSum.every(oldPair => {
+        let overlap = getOverlap(oldPair, newPair);
+
+        if (overlap) {
+            oldPair.start = newPair.start;
+            oldPair.end = newPair.end;
+            //console.log("Merged");
+            added = true;
+            return false;
+        } else {
+            return true;
+        }
+    });
+    
+    if (!added) {
+        pairSum.push(newPair);
+        //console.log("Added");
+    }
+}
+
+function getOverlap(pair1, pair2) {
+    if (
+        pair1.end + 1 > pair2.start - 1 &&
+        pair2.end + 1 > pair1.start - 1
+        ) {
+        return {
+            start: pair1.start < pair2.start ? pair1.start : pair2.start,
+            end: pair1.end > pair2.end ? pair1.end : pair2.end
+        };
+    }
+
+    return false;
+}
 
 /* const constraints = {
     min: formated.map(x => x.cx - x.br).sort((a, b) => a - b)[0],

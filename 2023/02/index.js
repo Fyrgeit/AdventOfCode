56d9df306988bedc28ff02f6fe58fe42ed0c658f
Fyrgeit <mans@peronilla.se>
2023-12-02 07:27:49 +0100

    dag 2

--- 2023/02/index.js
+++ 2023/02/index.js
@@ -1,3 +1,38 @@
 console.log((await Bun.file("input.txt").text())
-    
+    .split("\n")
+    .map(line => {
+        return {
+            i: Number(line.split(":")[0].slice(5)),
+            turns: line.split(": ")[1]
+            .split("; ")
+            .map(x => {
+                return x
+                .split(", ")
+                .map(n => {
+                    return {
+                        color: n.split(" ")[1],
+                        count: Number(n.split(" ")[0]),
+                    }
+                })
+            })
+        }
+    })
+    .map(game => {
+        let mins = {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        }
+
+        game.turns.forEach(turn => {
+            turn.forEach(cube => {
+                if (cube.count > mins[cube.color]) {
+                    mins[cube.color] = cube.count;
+                }
+            })
+        })
+
+        return mins["red"] * mins["green"] * mins["blue"];
+    })
+    .reduce((acc, val) => acc+val, 0)
 );

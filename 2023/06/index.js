b49fd58d068758895762f3622b51e291af22df6c
Fyrgeit <mans@peronilla.se>
2023-12-06 09:44:59 +0100

    day 6 doen

--- 2023/06/index.js
+++ 2023/06/index.js
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+const testRaces = { time: 71530, dist: 940200 };
+
+const races = { time: 55999793, dist: 401148522741405 };
+
+let race = races;
+let out = [];
+
+for (let i = 1; i < race.time; i++) {
+    let holdTime = i;
+    let goTime = race.time - i;
+    
+    let dist = goTime * holdTime;
+    out.push(dist);
+}
+    
+console.log(out.filter(dist => dist > race.dist).length);
